68653cdd597be82240a29d8b0171fdbea0614431
Alexandre Peter <contact@alexandrepeter.fr>
2022-03-18 15:30:34 +0100

    Add space

--- _utils/format.blade.php
+++ _utils/format.blade.php
@@ -4,6 +4,6 @@ function format_unit($unit){
     if (empty($unit) || ($unit === "pc")) {
         return null;
     }
-    return substr($unit, 0, 1);
+    return " " . substr($unit, 0, 1);
 }
 @endphp
